5c59358220de8edb1df8638082de96e043460c30
jhonega88 <jhonega88@hotmail.com>
2021-09-21 01:46:12 -0500

    Update main.tf

--- configuration/iaac/aks/kubernetes/main.tf
+++ configuration/iaac/aks/kubernetes/main.tf
@@ -1,38 +1,30 @@
 provider "azurerm" {
-   features {}
-}
-resource "azurerm_resource_group" "resource_group" {
-  name     = "${var.resource_group}_${var.environment}"
-  location = var.location
+  features {}
 }
 
-resource "azurerm_kubernetes_cluster" "terraform-k8s" {
-  name                = "${var.cluster_name}_${var.environment}"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  dns_prefix          = var.dns_prefix
+resource "azurerm_resource_group" "k8s" {
+  name     = "${var.prefix}-k8s-resources"
+  location = "${var.location}"
+}
 
-  linux_profile {
-    admin_username = "ubuntu"
-
-    ssh_key {
-      key_data = file(var.ssh_public_key)
-    }
-  }
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "${var.prefix}-k8s"
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
+  dns_prefix          = "${var.prefix}-k8s"
 
   default_node_pool {
-    name            = "agentpool"
-    node_count      = var.node_count
-    #vm_size         = "Standard_DS1_v2"
-	  vm_size         = "Standard_B2s"
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS1_v2"
   }
-
+  
   service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
+    client_id     = "${var.client_id}"
+    client_secret = "${var.client_secret}"
   }
 
   tags = {
-    Environment = var.environment
+    Environment = "Development"
   }
 }
