d8541cca1a9083bb2e8c48cebd0af7b43bfb8ee3
jhonega88 <jhonega88@hotmail.com>
2021-09-20 22:01:10 -0500

    Update main.tf

--- configuration/iaac/aks/kubernetes/main.tf
+++ configuration/iaac/aks/kubernetes/main.tf
@@ -2,46 +2,29 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "k8s" {
   name     = "${var.prefix}-k8s-resources"
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
+resource "azurerm_kubernetes_cluster" "k8s" {
   name                = "${var.prefix}-k8s"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
   dns_prefix          = "${var.prefix}-k8s"
 
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_DS1_v2"
+  }
+  
+  service_principal {
+    client_id     = "${var.client_id}"
+    client_secret = "{var.client_secret}"
   }
 
-  identity {
-    type = "SystemAssigned"
-  }
-
-  addon_profile {
-    aci_connector_linux {
-      enabled = false
-    }
-
-    azure_policy {
-      enabled = false
-    }
-
-    http_application_routing {
-      enabled = false
-    }
-
-    kube_dashboard {
-      enabled = true
-    }
-
-    oms_agent {
-      enabled = false
-    }
+  tags = {
+    Environment = "Development"
   }
 }
