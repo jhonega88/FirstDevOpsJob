5da4604421f03dfeef943907b2c5c83992248fe5
jhonega88 <jhonega88@hotmail.com>
2021-09-21 00:50:17 -0500

    Update variables.tf

--- configuration/iaac/aks/kubernetes/variables.tf
+++ configuration/iaac/aks/kubernetes/variables.tf
@@ -1,7 +1,30 @@
-variable "prefix" {
-  description = "k8sjhon"
+   
+variable client_id {}
+variable client_secret {}
+variable ssh_public_key {}
+
+variable environment {
+    default = "dev"
 }
 
-variable "location" {
-  description = "eastus"
+variable location {
+    default = "westeurope"
+}
+
+variable node_count {
+  default = 2
+}
+
+
+
+variable dns_prefix {
+  default = "k8sAforo255sv"
+}
+
+variable cluster_name {
+  default = "k8sAforo255sv"
+}
+
+variable resource_group {
+  default = "kubernetesAforo255"
 }
